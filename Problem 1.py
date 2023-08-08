7aee8db805be52240f9820f779dfb8d0313a2a4e
mikaellasharma <82779072+mikaellasharma@users.noreply.github.com>
2023-08-07 21:44:38 -0700

    add

--- Problem 1.py
+++ Problem 1.py
@@ -1,16 +1,20 @@
  # 1. Two Sum 
 Language in Python3
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        a=0
-        for i in range(len(nums)):
-            p = target-nums[i]
-            if p in nums:
-                a=nums.index(p)
-                if a==i:
-                    continue
-                break
-        return i,a
+class Solution(object):
 
+    def twoSum(self, nums, target):
+        # two point
+        nums_index = [(v, index) for index, v in enumerate(nums)]
+        nums_index.sort()
+        begin, end = 0, len(nums) - 1
+        while begin < end:
+            curr = nums_index[begin][0] + nums_index[end][0]
+            if curr == target:
+                return [nums_index[begin][1], nums_index[end][1]]
+            elif curr < target:
+                begin += 1
+            else:
+                end -= 1
+                
 
