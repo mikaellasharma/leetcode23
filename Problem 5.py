2a66e84a45927dc3b04751ea2f80bf091af5bff6
mikaellasharma <82779072+mikaellasharma@users.noreply.github.com>
2023-08-07 21:52:52 -0700

    Longest Palindromic Substring

--- Problem 5.py
+++ Problem 5.py
@@ -1,41 +1,21 @@
 # 5. Longest Palindromic Substring
 
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        ls = len(s)
-        if ls <= 1 or len(set(s)) == 1:
-            return s
-        
-        temp_s = '#'.join('{}'.format(s))
-     
-        tls = len(temp_s)
-        seed = range(1, tls - 1)
-       
-        len_table = [0] * tls
-        for step in range(1, tls / 2 + 1):
-            final = []
-            for pos in seed:
-                if pos - step < 0 or pos + step >= tls:
-                    continue
-                if temp_s[pos - step] != temp_s[pos + step]:
-                    continue
-                final.append(pos)
-                if temp_s[pos - step] == '#':
-                    continue
-                len_table[pos] = step
-            seed = final
-        max_pos, max_step = 0, 0
-        for i, s in enumerate(len_table):
-            if s >= max_step:
-                max_step = s
-                max_pos = i
-        return temp_s[max_pos - max_step:max_pos + max_step + 1].translate(None, '#')
+class Solution:
 
-if __name__ == '__main__':
-    s = Solution()
-    print(s.longestPalindrome("abcbe"))
-
+    def longestPalindrome(self, s: str) -> str:
+        length = len(s)
+        palindrome = ''
+        palindrome_length = 0
+        for i in range(length):
+            j = i+1
+            while j <= length:
+                if self.isPalindrome(s[i:j]) and palindrome_length < len(s[i:j]):
+                    palindrome = s[i:j]
+                    palindrome_length = len(s[i:j])
+                    if palindrome_length == length:
+                        return palindrome
+                j += 1
+        return palindrome
+    
+    def isPalindrome(self, s):
+        return s == s[::-1]
